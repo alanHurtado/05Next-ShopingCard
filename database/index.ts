1511f1d2658701008c623bd98f10df77d82ef429
Alan Astroga Hurtado <alanasto10@gmail.com>
2022-08-04 15:17:01 -0500

    carga de querys api

---
+++ database/index.ts
@@ -0,0 +1,7 @@
+export * as db from "./db";
+export * from "./conatants";
+
+
+
+//Base de datos de prueba
+export * as seedDatabase from "./products";
--- interfaces/product.ts
+++ interfaces/product.ts
@@ -1,16 +1,20 @@
 export interface IProduct {
-    description: string;
-    images: string[];
-    inStock: number;
-    price: number;
-    sizes: IValidSizes[];
-    slug: string;
-    tags: string[];
-    title: string;
-    type: IValidTypes;
-    gender: 'men'|'women'|'kid'|'unisex'
+  _id: string;
+  description: string;
+  images: string[];
+  inStock: number;
+  price: number;
+  sizes: IValidSizes[];
+  slug: string;
+  tags: string[];
+  title: string;
+  type: IValidTypes;
+  gender: "men" | "women" | "kid" | "unisex";
+
+  // TODO: agregar createAT y updateAt
+  createdAt: string;
+  updatedAt: string;
 }
 
-
-export type IValidSizes = 'XS'|'S'|'M'|'L'|'XL'|'XXL'|'XXXL';
-export type IValidTypes = 'shirts'|'pants'|'hoodies'|'hats';
+export type IValidSizes = "XS" | "S" | "M" | "L" | "XL" | "XXL" | "XXXL";
+export type IValidTypes = "shirts" | "pants" | "hoodies" | "hats";
