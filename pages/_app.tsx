16b8413cad8637fd564df1a0cccbd8c2960d54c8
Alan Astroga Hurtado <alanasto10@gmail.com>
2022-09-01 16:59:51 -0500

    se agrego paypal solo la logica ya que las llaves y registro no se efectuo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,26 +6,30 @@ import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { AuthProvider, CartProvider, UiProvider } from "../context";
 
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider>
-      <SWRConfig
-        value={{
-          // refreshInterval: 3000,
-          fetcher: (resource, init) => fetch(resource, init).then((res) => res.json()),
-        }}
-      >
-        <UiProvider>
-          <AuthProvider>
-            <CartProvider>
-              <ThemeProvider theme={lightTheme}>
-                <CssBaseline />
-                <Component {...pageProps} />
-              </ThemeProvider>
-            </CartProvider>
-          </AuthProvider>
-        </UiProvider>
-      </SWRConfig>
+      <PayPalScriptProvider options={{ "client-id": "test" || "" }}>
+        <SWRConfig
+          value={{
+            // refreshInterval: 3000,
+            fetcher: (resource, init) => fetch(resource, init).then((res) => res.json()),
+          }}
+        >
+          <UiProvider>
+            <AuthProvider>
+              <CartProvider>
+                <ThemeProvider theme={lightTheme}>
+                  <CssBaseline />
+                  <Component {...pageProps} />
+                </ThemeProvider>
+              </CartProvider>
+            </AuthProvider>
+          </UiProvider>
+        </SWRConfig>
+      </PayPalScriptProvider>
     </SessionProvider>
   );
 }
